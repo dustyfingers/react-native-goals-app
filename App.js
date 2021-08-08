3d1f0b7f1968bce82c9035237b9ca10515498ce0
lou <dustyfingers01257@gmail.com>
2021-08-07 23:45:50 -0600

    learned about the core components in react native

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.pageContainer}>
+      <View>
+        <TextInput placeholder="Say something!" style={styles.textInput}/>
+        <Button title="Add"/>
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  pageContainer: {
+    padding: 30
   },
+  textInput: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
+    marginBottom: 30,
+    marginTop: 30,
+    paddingBottom: 10
+  }
 });
