f5be3ff0b36e11eefa09dd67cdd0b159059f58f2
lou <dustyfingers01257@gmail.com>
2021-08-08 07:45:44 -0600

    learned about stylesheet objects in react native

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const handlePress = () => {
+    console.log('handlePress called!')
+  }
   return (
     <View style={styles.pageContainer}>
-      <View>
+      <View style={styles.inputContainer}>
         <TextInput placeholder="Say something!" style={styles.textInput}/>
-        <Button title="Add"/>
+        <Button title="Add" onPress={handlePress}/>
       </View>
       <View>
 
@@ -20,7 +23,12 @@ const styles = StyleSheet.create({
   pageContainer: {
     padding: 30
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
   textInput: {
+    width: 200,
     borderBottomColor: 'black',
     borderBottomWidth: 2,
     marginBottom: 30,
